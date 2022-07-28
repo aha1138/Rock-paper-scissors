92b3636c9665242d26c11da8e61f0aee68eff499
aha1138 <ahadgujar22@gmail.com>
2022-07-28 15:45:02 -0400

    Play game with buttons
    Added 3 buttons for the user to choose rock paper or scissors

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+const selections = document.querySelectorAll('[data-selection]');
 
 //function that returns Rock, paper, scissors randomly
 function computerPlay() {
@@ -14,16 +15,26 @@ function computerPlay() {
         return "scissors";
     }
 }  
+
+//function that lets the player choose rock, paper or scissors
+selections.forEach(selection => {
+    selection.addEventListener('click', e => {
+        const playerPlay = selection.dataset.selection; 
+        playRound(playerPlay);
+    })
+})
  
 //function that plays one round
-function playRound (playerSelection,computerSelection){
+function playRound(playerPlay){
+    
+    let computerSelection = computerPlay();
 
-    console.log("Player choice: " + playerSelection);
-    console.log("Computer choice: " + computerSelection);
+    console.log("Player selection: " + playerPlay);
+    console.log("Computer selection: " + computerSelection);
     console.log("Player: " + playerScore + " - " +"Computer: " + computerScore);
 
     //Player selection is rock
-    if (playerSelection.toLowerCase() === "rock" ){
+    if (playerPlay === "rock" ){
        if (computerSelection === "rock"){
         return "Tie! Try Again!";
     } else if (computerSelection === "paper"){
@@ -34,7 +45,7 @@ function playRound (playerSelection,computerSelection){
         return "You Win! Rock Beats Scissors!";}
     }
 //Player selection is paper
-    if (playerSelection.toLowerCase() === "paper" ){
+    if (playerPlay === "paper" ){
         if (computerSelection === "rock"){
         playerScore++;
          return "You Win! Paper Beats Rock!";
@@ -45,7 +56,7 @@ function playRound (playerSelection,computerSelection){
         return "You Lose Scissors Beats Paper!";}
      }
 //Player Selection is scissors  
-     if (playerSelection.toLowerCase() === "scissors" ){
+     if (playerPlay === "scissors" ){
         if (computerSelection === "rock"){
         computerScore++;
         return "You Lose! Rock Beats Scissors!";
@@ -58,37 +69,20 @@ function playRound (playerSelection,computerSelection){
     }
 function game(){ 
 
-//function that lets the player choose rock, paper or scissors
-     function playerPlay(){
-       let playerChoice = prompt("Rock, paper, scissors?");
-       while (playerChoice == null){
-        prompt("Rock, paper, scissors?");
-       }
-         if (playerChoice.toLocaleLowerCase() === "rock" || parseInt(playerChoice)=== 0){
-             playerChoice="Rock";}
-         else if (playerChoice.toLocaleLowerCase()=== "paper" || parseInt(playerChoice)=== 1){
-             playerChoice= "Paper"}
-         else if (playerChoice.toLocaleLowerCase() === "scissors" || parseInt(playerChoice) === 2) {
-             playerChoice= "Scissors"}
-         else {
-             console.log("You made a mistake! Correct it RIGHT NOW!!!")
-         }
-       
-       return playerChoice;}
+// //function that plays 5 games
+// for (let round = 1; round < 6; round++){
+//     console.log("ROUND: " + (round));
+//     const playerPlay = playerPlay();
+//     const computerSelection = computerPlay();
+//     const rounds = playRound (playerPlay, computerSelection);
+//     console.log(rounds);
+//     console.log("--------------------");
+    
+//  };
+//  while (round = 5){
+//      return (winner())
+//  }
 
-//function that plays 5 games
-    for (let round = 1; round < 6; round++){
-        console.log("ROUND: " + (round));
-        const playerSelection = playerPlay();
-        const computerSelection = computerPlay();
-        const rounds = playRound (playerSelection, computerSelection);
-        console.log(rounds);
-        console.log("--------------------");
-        
-     };
-     while (round = 5){
-        return (winner())
-     }
 
 
 function winner(){
